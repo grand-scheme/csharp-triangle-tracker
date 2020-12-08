670d2caecf73fbc1b3166aee290e74e969080169
Shannon Grantski <grantski@pm.me>
2020-12-08 10:34:08 -0800

    add constructor for class Triangle.
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- Triangle.Solution/Triangle/Models/Triangle.cs
+++ Triangle.Solution/Triangle/Models/Triangle.cs
@@ -1,7 +1,16 @@
 namespace Triangle
 {
-  public class ClassTriangle
+  public class Triangle
   {
-    // etc
+    public int SideOne { get; set; }
+    public int SideTwo { get; set; }
+    public int SideThree { get; set; }
+
+    public Triangle(int sideOne, int sideTwo, int sideThree)
+    {
+      SideOne = sideOne;
+      SideTwo = sideTwo;
+      SideThree = sideThree;
+    }
   }
 }
