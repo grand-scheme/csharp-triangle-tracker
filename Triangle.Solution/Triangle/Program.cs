29a41ff817652e011fb19120cd613bf5756d54c9
Shannon Grantski <grantski@pm.me>
2020-12-08 13:21:10 -0800

    start program.cs with intial console.write/read
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

---
+++ Triangle.Solution/Triangle/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Triangle
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Please input value for first side of triangle");
+      int side1 = int.Parse(Console.ReadLine());
+      Console.WriteLine(side1);
+    }
+  }
+}
