27c217ee6860f7e49ecbe46e75ea823eb00a0be1
Shannon Grantski <grantski@pm.me>
2020-12-08 13:39:29 -0800

    finalize program.cs to run triangle calculations.
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- Triangle.Solution/Triangle/Program.cs
+++ Triangle.Solution/Triangle/Program.cs
@@ -10,7 +10,32 @@ namespace Triangle
     {
       Console.WriteLine("Please input value for first side of triangle");
       int side1 = int.Parse(Console.ReadLine());
-      Console.WriteLine(side1);
+      Console.WriteLine("Please input value for second side of triangle");
+      int side2 = int.Parse(Console.ReadLine());
+      Console.WriteLine("Please input value for third side of triangle");
+      int side3 = int.Parse(Console.ReadLine());
+      Console.WriteLine(side1 + ", " + side2 + ", " + side3);
+
+      Triangle triangle = new Triangle(side1, side2, side3);
+      if (triangle.IsTriangle(side1, side2, side3))
+      {
+        if (triangle.IsEquilateral(side1, side2, side3))
+        {
+          Console.WriteLine("It is equilateral triangle");
+        } 
+        else if (triangle.IsIsosceles(side1, side2, side3))
+        {
+          Console.WriteLine("It is an isosceles triangle");
+        } 
+        else if (triangle.IsScalene(side1, side2, side3))
+        {
+          Console.WriteLine("It is a scalene triangle");
+        } 
+      }
+      else 
+      {
+        Console.WriteLine("It is not a triangle");
+      }
     }
   }
 }
